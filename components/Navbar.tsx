c1dffb8e39458373c19a11b24fa32b4ad18fc053
Prabhat <prabhatdwivedi224@gmail.com>
2024-04-04 03:08:46 +0530

    Auth implemented

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
-
-const Navbar = () => {
+import Link from 'next/link'
+import Image from 'next/image'
+import Mobilenav from './Mobilenav'
+import { SignedIn,UserButton } from '@clerk/nextjs'
+const Navbar = () =>{
   return (
-    <div>
-      Navbar
+    <div className='flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10'>
+      <Link href='/'>
+        <Image src='../icons/yoom-logo.svg' alt='Logo' width={100} height={100}/ >
+      </Link>
+      <div className='flex-between gap-5'>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+        <Mobilenav/>
+      </div>
     </div>
   )
 }
